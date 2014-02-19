bf1f1e341789113d1a6a33df2adb042d240c612c
Denise Fernandez <dcbfernandez@gmail.com>
2014-02-19 23:56:55 +1100

    Removing menu selection feature, can add back if we need to

--- src/main/java/au/com/denisefernandez/Main.java
+++ src/main/java/au/com/denisefernandez/Main.java
@@ -11,25 +11,8 @@ public class Main {
     private static final PrintStream out = System.out;
     private static final Game game = new Game();
 
-    private static final String[] primaryMenuOptions = new String[]  {"[1] Start a new 2 player game", "[quit] Quit this program"};
-
     public static void main(String[] args) {
         out.println("*** NOUGHTS AND CROSSES ***");
-        out.println("Welcome to Noughts and Crosses, please choose one of the following options:");
-        printPrimaryMenuOptions();
-        String menuSelection = in.nextLine();
-
-        if (menuSelection.equals("1")) {
-            startNewGame();
-        } else if (menuSelection.trim().equalsIgnoreCase("quit")) {
-            System.exit(0);
-        } else {
-            out.println("Your selection '" + menuSelection + " was not recognised. Please choose one of the following options:");
-            printPrimaryMenuOptions();
-        }
-    }
-
-    private static void startNewGame() {
         addPlayersToGame();
         out.println(game.getCurrentGameState());
         while(game.isRunning()) {
@@ -69,11 +52,5 @@ public class Main {
         out.println("Please enter the name of Player 2 [X]: ");
         String player2Name = in.nextLine();
         game.addPlayers(player1Name, player2Name);
-    }
-
-    private static void printPrimaryMenuOptions() {
-        for (String option : primaryMenuOptions) {
-            out.println(option);
-        }
     }
 }
